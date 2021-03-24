38e24f334119d3a2785981624df47a82dd3126a1
Crowdbotics <team@crowdbotics.com>
2021-03-24 23:13:44 +0000

    Update models

--- backend/novice_lodge_25249/wsgi.py
+++ backend/novice_lodge_25249/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'novice_lodge_25249.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "novice_lodge_25249.settings")
 
 application = get_wsgi_application()
