1042205612e0143af27f7991b8bd3206d7922adf
Crowdbotics <team@crowdbotics.com>
2021-03-24 19:40:17 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/novice_lodge_25249/MainActivity.java
@@ -0,0 +1,15 @@
+package com.novice_lodge_25249;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "novice_lodge_25249";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://novice-lodge-25249.botics.co",
+  defaultTimeout: 5000
+};
